201e27908abd69502abb54164c1bae6bd4510da9
AkshayV30 <akshayatwork.v30@gmail.com>
2025-04-01 01:09:10 +0530

    frontend deploy.6

--- to_do_app_crud_with_pern_stack__client/vite.config.js
+++ to_do_app_crud_with_pern_stack__client/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/to-do-app/to_do_app_crud_with_pern_stack__client/",
+  base: "/to-do-app/",
 });
