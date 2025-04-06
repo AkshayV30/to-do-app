81b75967552f3aaa3f44fd9e184e19d67a1e0813
AkshayV30 <akshayatwork.v30@gmail.com>
2025-04-07 00:50:14 +0530

    debug backend deploy.12

--- to_do_app_crud_with_pern_stack__server/middlewares/middlewares.js
+++ to_do_app_crud_with_pern_stack__server/middlewares/middlewares.js
@@ -5,21 +5,23 @@ import express from "express";
 // import compression from "compression";
 // import morgan from "morgan";
 
-const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:5173"];
+// const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:5173"];
 
 console.log("Allowed Origins:", allowedOrigins);
 
 export const configMiddleware = (app) => {
   // CORS
-  app.use(
-    cors({
-      origin: allowedOrigins,
-      methods: "GET,POST,PUT,DELETE",
-      allowedHeaders: "Content-Type",
-    })
-  );
+  // app.use(
+  //   cors({
+  //     origin: allowedOrigins,
+  //     methods: "GET,POST,PUT,DELETE",
+  //     allowedHeaders: "Content-Type",
+  //   })
+  // );
 
   // app.options("*", cors());
+
+  app.use(cors("*"));
 
   // Parse JSON bodies
   app.use(express.json());
