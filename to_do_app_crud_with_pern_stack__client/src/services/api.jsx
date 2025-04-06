efd1de900d6e2e63523eff50417e0724e085cb83
AkshayV30 <akshayatwork.v30@gmail.com>
2025-04-07 00:24:43 +0530

    debug backend deploy.7

--- to_do_app_crud_with_pern_stack__client/src/services/api.jsx
+++ to_do_app_crud_with_pern_stack__client/src/services/api.jsx
@@ -7,22 +7,31 @@ const API_BASE_URL =
 // const API_BASE_URL = "https://to-do-app-ragl.onrender.com/v1/todos";
 console.log("API Base URL:", API_BASE_URL);
 
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
 export const getTodos = async () => {
-  const response = await axios.get(API_BASE_URL);
+  // const response = await axios.get(API_BASE_URL);
+  const response = await api.get("/");
   return response.data;
 };
 
 export const addTodo = async (description) => {
-  const response = await axios.post(API_BASE_URL, { description });
+  // const response = await axios.post(API_BASE_URL, { description });
+
+  const response = await api.post("/", { description });
   return response.data;
 };
 
 export const updateTodo = async (id, description) => {
-  const response = await axios.put(`${API_BASE_URL}/${id}`, { description });
-
+  // const response = await axios.put(`${API_BASE_URL}/${id}`, { description });
+  const response = await api.put(`/${id}`, { description });
   return response.data;
 };
 
 export const deleteTodo = async (id) => {
-  return axios.delete(`${API_BASE_URL}/${id}`);
+  // return axios.delete(`${API_BASE_URL}/${id}`);
+  const response = await api.delete(`/${id}`);
+  return response.data;
 };
