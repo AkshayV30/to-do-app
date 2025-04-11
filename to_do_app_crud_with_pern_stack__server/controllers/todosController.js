01d3d2ad56c64ae05a84d7ffe12bec408eaed551
AkshayV30 <akshayatwork.v30@gmail.com>
2025-04-11 22:07:12 +0530

    debug backend deploy.28

--- to_do_app_crud_with_pern_stack__server/controllers/todosController.js
+++ to_do_app_crud_with_pern_stack__server/controllers/todosController.js
@@ -5,16 +5,6 @@ console.log(" Using Neon DB:", isUsingNeon);
 const query = async (queryText, values = []) => {
   try {
     if (isUsingNeon) {
-      // let textWithValues = queryText;
-
-      // Neon doesn't use $1-style placeholders in `.unsafe`, so do direct substitution carefully
-      // const parts = queryText.split(/\$\d+/);
-      // const taggedQuery = parts.reduce((acc, part, i) => {
-      //   if (i === 0) return [part];
-      //   return [...acc, values[i - 1], part];
-      // }, []);
-
-      // const result = await sql(...taggedQuery);
       const result = await sql.query(queryText, values);
       console.log(" Neon SQL result:", result);
 
@@ -30,9 +20,6 @@ const query = async (queryText, values = []) => {
   }
 };
 
-// const extractRows = (result) => (isUsingNeon ? result : result.rows);
-// const extractOne = (result) => (isUsingNeon ? result[0] : result.rows?.[0]);
-
 const extractRows = (result) => result;
 const extractOne = (result) => result[0];
 
@@ -40,11 +27,7 @@ const extractOne = (result) => result[0];
 export const createTodo = async (req, res) => {
   try {
     const { description } = req.body;
-    // const newTodo = await pool.query(
-    //   "INSERT INTO todo (description) VALUES($1) RETURNING *",
-    //   [description]
-    // );
-    // res.json(newTodo.rows[0]);
+
     const result = await query(
       `INSERT INTO todo (description) VALUES($1) RETURNING *`,
       [description]
@@ -60,13 +43,10 @@ export const createTodo = async (req, res) => {
 // Get all Todos
 export const getTodos = async (req, res) => {
   try {
-    // const allTodos = await pool.query("SELECT * FROM todo");
-    // res.json(allTodos.rows);
     const result = await query(`SELECT * FROM todo`, []);
 
     console.log(" Raw query result:", result);
-    // const todos = extractRows(result);
-    // res.json(todos);
+
     res.json(result);
   } catch (err) {
     console.error("Get Todos Error:", err.message);
@@ -78,10 +58,6 @@ export const getTodos = async (req, res) => {
 export const getTodoById = async (req, res) => {
   try {
     const { id } = req.params;
-    // const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
-    //   id,
-    // ]);
-    // res.json(todo.rows[0]);
 
     const result = await query(`SELECT * FROM todo WHERE todo_id = $1`, [id]);
     const todo = extractOne(result);
@@ -104,17 +80,9 @@ export const updateTodo = async (req, res) => {
     const { id } = req.params;
     const { description } = req.body;
 
-    // console.log("🔍 Received update request for ID:", id);
-    // console.log("🔍 New Description:", description);
-
     if (!description) {
       return res.status(400).json({ error: "Description is required" });
     }
-
-    // const updatedTodo = await pool.query(
-    //   "UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *",
-    //   [description, id]
-    // );
 
     const result = await query(
       `UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *`,
@@ -130,7 +98,7 @@ export const updateTodo = async (req, res) => {
 
     res.json({
       message: "Todo updated successfully!",
-      // todo: updatedTodo.rows[0],
+
       todo: updatedTodo,
     });
   } catch (err) {
@@ -143,7 +111,6 @@ export const updateTodo = async (req, res) => {
 export const deleteTodo = async (req, res) => {
   try {
     const { id } = req.params;
-    // await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
 
     const result = await query(`DELETE FROM todo WHERE todo_id = $1`, [id]);
 
